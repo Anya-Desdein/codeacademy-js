5fedb85e9a138b1f54e79622901f2ce87660f273
unknown <heavelyn1999@gmail.com>
2020-08-04 22:31:01 +0200

    Rock paper scissors - If statement simplification

--- 04-08-1-Rock, Paper, or Scissors.js
+++ 04-08-1-Rock, Paper, or Scissors.js
@@ -8,54 +8,40 @@ const validChoicesDict = {
 
 const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
+  
   if (userInput in validChoicesDict) {
     return userInput;
   }
+
   console.log('Wrong user input.');
 };
 
 const getComputerChoice = () => {
   const roll = Math.floor(Math.random() * 3);
 
-  if (roll === 0) {
-    return 'rock';
-  } else if (roll === 1) {
-    return 'paper';
-  } else if (roll === 2) {
-    return 'scissors';
-  }
+  const computerOptions = [
+    'rock', 'paper', 'scissors'
+  ];
+
+  return computerOptions[roll];
 };
 
-const determineWinner = (userChoice, computerChoice) => {
+const determineWinMessage = (userChoice, computerChoice) => {
   if (userChoice === 'bomb') {
     return 'you win';
+  }
+
+  if (userChoice === computerChoice) {
+    return 'tie';
   } else {
-    if (userChoice === computerChoice) {
-      return 'tie';
-    }  else {
-      if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-          return 'you loose';
-        } else {
-          return 'you win';
-        }
-      }
-
-      if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-          return 'you loose';
-        } else {
-          return 'you win';
-        }
-      }
-
-      if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-          return 'you loose';
-        } else {
-          return 'you win';
-        }
-      }
+    if (userChoice === 'rock') {
+      return computerChoice === 'paper' ? 'you loose' : 'you win';
+    }else if (userChoice === 'paper') {
+      return computerChoice === 'scissors' ? 'you loose' : 'you win';
+    }else if (userChoice === 'scissors') {
+      return computerChoice === 'rock' ? 'you loose' : 'you win';
+    }else{
+      throw new Error(`Unexpected choice: ${userChoice}`);
     }
   }
 };
@@ -65,7 +51,7 @@ const playGame = () => {
   const computerChoice = getComputerChoice();
   console.log('You threw: ' + userChoice);
   console.log('The computer threw: ' + computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
+  console.log(determineWinMessage(userChoice, computerChoice));
 };
 
 playGame();
