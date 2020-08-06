f62ad355bfdc3162efcb955ebfb94f71266ddfd6
Heavelyn <heavelyn1999@gmail.com>
2020-08-06 21:49:33 +0200

    Sleep debt calculator - added ideal time prompts

--- sleep-debt-calculator/04-08-2-Sleep Debt Calculator.js
+++ sleep-debt-calculator/04-08-2-Sleep Debt Calculator.js
@@ -1,3 +1,5 @@
+const prompts = require('prompts');
+
 const dayOfTheWeekSleep = {
   monday: 6,
   tuesday: 4,
@@ -7,34 +9,61 @@ const dayOfTheWeekSleep = {
   saturday: 8,
   sunday: 8,
 };
-const validChoicesArray = Object.keys(dayOfTheWeekSleep);
+const dayOfTheWeekNames = Object.keys(dayOfTheWeekSleep);
 
-const getSleepHours = (day) => {
-  day = day.toLowerCase();
-  if (day in dayOfTheWeekSleep) {
-    return dayOfTheWeekSleep[day];
-  }
-  console.log(`Wrong user input.`);
+const validateHourPromptFn = val => {
+  if(val === "") return true;
+  if(val < 1) return "You don't like to sleep well. Enter at least one hour.";
+  else if(val > 20) return "This program doesn't support hibernating species. Please enter value lower than 20.";
+  else return true;
 };
 
 const getActualSleepHoursCurrentWeek = () => {
-  let sleepHoursWeek = 0;
-  for (let i = 0; i < 7; i++) {
-    const day = validChoicesArray[i];
-    sleepHoursWeek += getSleepHours(day);
+    let sleepHoursWeek = 0;
+    for (let i = 0; i < 7; i++) {
+      const day = dayOfTheWeekNames[i];
+      sleepHoursWeek += dayOfTheWeekSleep[day];
+    }
+    return sleepHoursWeek;
+};
+
+const getidealSleepHoursPerWeek = async () => {
+  const { provideValuesSeparately } = await prompts({
+    type: 'select',
+    name: 'provideValuesSeparately',
+    message: 'Would you like to specify your ideal sleep duration for each day of the day separately?',
+    choices: [
+      { title: 'Average daily sleep hours', value: false },
+      { title: 'Enter each day individually', value: true },
+    ]
+  });
+
+  if(provideValuesSeparately) {
+    const replies = await prompts(dayOfTheWeekNames.map(dayName => ({
+      type: 'number',
+      name: dayName,
+      message: `Enter your ideal sleep duration for ${dayName}`,
+      initial: 7.5,
+      validate: validateHourPromptFn
+    })));
+
+    return Object.values(replies).reduce((prev, curr) => prev + curr);
+  }else{
+    const {value} = await prompts({
+      type: 'number',
+      name: 'value',
+      message: 'Enter your ideal daily sleep duration in hours',
+      initial: 7.5,
+      validate: validateHourPromptFn
+    });
+    
+    return value * 7;
   }
-  return sleepHoursWeek;
 };
 
-const getidealSleepHoursPerWeek = () => {
-  const idealHours = 7.5;
-  return idealHours * 7;
-};
-
-const calculateSleepDebt = () => {
+const calculateSleepDebt = async () => {
   const actualSleepHours = getActualSleepHoursCurrentWeek();
-  const idealSleepHours = getidealSleepHoursPerWeek();
-
+  const idealSleepHours = await getidealSleepHoursPerWeek();
   if (idealSleepHours === actualSleepHours) {
     console.log(`Spałeś ${actualSleepHours}, jest to idealna wartosc.`)
   } else if (actualSleepHours > idealSleepHours) {
@@ -45,6 +74,3 @@ const calculateSleepDebt = () => {
 }
 
 calculateSleepDebt();
-
-
-
